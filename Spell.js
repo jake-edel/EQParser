c7a56a60fa85c7685b587d73cc78059e7a9ad69e
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-19 08:21:02 -0500

    refactor spell logic

--- Spell.js
+++ Spell.js
@@ -5,7 +5,9 @@ class Spell {
     this.spellSignatures = {
       deadeye: 'Your vision shifts.',
       shadow_sight: 'The shadows fade.',
-      gather_shadows: 'You gather shadows about you.'
+      gather_shadows: 'You gather shadows about you.',
+      malignant_dead: 'At your service master',
+      augment_death: 'gleam with madness'
     }
     this.spellCastSignatures = [
       'You begin casting',
@@ -16,7 +18,6 @@ class Spell {
   }
 
   isSpellCast(line) {
-    // this.debugLog('Checking Spell Cast:', line);
     const currentSpellId = this.currentSpell?.toLowerCase().replace(/ /g, '_');
 
     const isSpellCast = this.spellCastSignatures.some(signature => line?.includes(signature))
@@ -29,44 +30,49 @@ class Spell {
     return line?.includes('You begin casting');
   }
 
+  setCurrentSpell(line) {
+    if (this.isNewCast(line)) {
+      const spellName = line.split('You begin casting ')[1].split('.').shift();
+      gameState.currentSpell = spellName;
+    }
+  }
+
   isInterrupted(line) {
     return line?.includes('Your spell is interrupted');
+  }
+
+  setSpellInterrupted() {
+    gameState.currentSpell = 'INTERRUPTED!';
+    setTimeout(() => gameState.currentSpell = null, 2000);
   }
 
   isFizzle(line) {
     return line?.includes('Your spell fizzles!');
   }
+  setSpellFizzle() {
+    gameState.currentSpell = 'FIZZLE!';
+    setTimeout(() => gameState.currentSpell = null, 2000);
+  }
+
+  isSpellComplete(line) {
+    const spellId = gameState.currentSpell?.toLowerCase().replace(/ /g, '_');
+    const spellSignature = this.spellSignatures[spellId];
+    return spellId && line?.includes(spellSignature);
+  }
 
   handleSpellCast(line) {
-    if (this.isNewCast(line)) {
-      const spellName = line.split('You begin casting ')[1].split('.').shift();
-      gameState.currentSpell = spellName;
-      // this.debugLog('Current Spell:', gameState.currentSpell);
-      return
-    }
+    if (this.isNewCast(line)) return this.setCurrentSpell(line)
 
-    if (this.isInterrupted(line)) {
-      // this.debugLog('Spell Interrupted:', gameState.currentSpell);
-      gameState.currentSpell = 'INTERRUPTED!';
-      setTimeout(() => gameState.currentSpell = null, 2000);
-      return
-    }
+    if (this.isInterrupted(line)) return this.setSpellInterrupted(line)
 
-    if (this.isFizzle(line)) {
-      // this.debugLog('Spell Fizzled:', gameState.currentSpell);
-      gameState.currentSpell = 'FIZZLE!';
-      setTimeout(() => gameState.currentSpell = null, 2000);
-      return
-    }
+    if (this.isFizzle(line)) return this.setSpellFizzle()
+
+    if(this.isSpellComplete()) return this.setSpellComplete() 
 
     const spellId = gameState.currentSpell?.toLowerCase().replace(/ /g, '_');
     const spellSignature = this.spellSignatures[spellId];
     if (!spellSignature) this.debugLog('UNKNOWN SPELL:', gameState.currentSpell);
-    if (spellId && line?.includes(spellSignature)) {
-      // this.debugLog('Spell Complete: ', gameState.currentSpell);
-      gameState.currentSpell = null;
-      return
-    }
+    if (spellId && line?.includes(spellSignature)) gameState.currentSpell = null;
   }
 
 }
