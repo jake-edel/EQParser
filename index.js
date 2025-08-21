e9ce4e0f21ceb836b8f1d6e72fddce8247d580d3
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-21 17:38:28 -0500

    move script into separate file

---
+++ index.js
@@ -0,0 +1,38 @@
+    const serverPort = 4000
+    const ws = new WebSocket(`ws://localhost:${serverPort}`);
+
+    ws.onopen = () => {
+      console.log(`WebSocket connection established with port ${serverPort}`);
+      ws.send('Hello from the client!');
+    };
+    
+    ws.onmessage = event => {
+      event.data.text().then(text => {
+        console.log(JSON.parse(text));
+        const message = JSON.parse(text);
+        const keys = Object.keys(message);
+        keys.forEach(key => {
+          if (key === 'location') handleLocation(message[key]);
+          if (key === 'debug') handleDebug(message[key]);
+        });
+      });
+    }
+    function handleDebug(debugMessage) {
+      const id = 'debug';
+      const innerText = `Debug: ${debugMessage}`;
+      const element = createElement(id, innerText);
+      document.getElementById('dashboard').prepend(element);
+    }
+
+    function handleLocation(location) {
+      const id = 'location';
+      const innerText = `Location - X: ${location.x}, Y: ${location.y}, Z: ${location.z}`;
+      document.getElementById('location').innerText = innerText;
+    }
+
+    function createElement(id, innerText) {
+      const element = document.createElement('div');
+      element.id = id;
+      element.innerText = innerText;
+      return element;
+    }
