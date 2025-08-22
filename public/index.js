bd33126a2e70584a722a166aebd3b532b515e94f
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-21 21:33:10 -0500

    add more datapoints to dashboard

--- public/index.js
+++ public/index.js
@@ -1,41 +1,46 @@
     const serverPort = 4000
     const ws = new WebSocket(`ws://192.168.1.79:${serverPort}`);
 
-    ws.onopen = () => {
-      console.log(`WebSocket connection established with port ${serverPort}`);
-      ws.send('Hello from the client!');
-    };
-    
     ws.onmessage = event => {
-      console.log(event)
-      // If we're getting a string back from the server
-      // handle it as is, no buffer to deal with
       if (typeof event.data === 'string') {
         console.log(event.data);
         return
       }
 
       event.data.text().then(text => {
-        console.log(JSON.parse(text));
         const message = JSON.parse(text);
         const keys = Object.keys(message);
         keys.forEach(key => {
           if (key === 'location') handleLocation(message[key]);
           if (key === 'debug') handleDebug(message[key]);
+          if (key === 'compassDirection') handleCompassDirection(message[key]);
+          if (key === 'petName') handlePetName(message[key]);
+          if (key === 'petStatus') handlePetStatus(message[key]);
         });
       });
     }
     function handleDebug(debugMessage) {
       const id = 'debug';
-      const innerText = `Debug: ${debugMessage}`;
+      const innerText = `${debugMessage}`;
       const element = createElement(id, innerText);
       document.getElementById('dashboard').prepend(element);
     }
 
     function handleLocation(location) {
-      const id = 'location';
-      const innerText = `Location - X: ${location.x}, Y: ${location.y}, Z: ${location.z}`;
+      const innerText = `X: ${location.x}, Y: ${location.y}`;
       document.getElementById('location').innerText = innerText;
+    }
+
+    function handleCompassDirection(direction) {
+      document.getElementById('compassDirection').innerText = direction;
+    }
+
+    function handlePetName(petName) {
+      document.getElementById('petName').innerText = petName;
+    }
+
+    function handlePetStatus(petStatus) {
+      document.getElementById('petStatus').innerText = petStatus;
     }
 
     function createElement(id, innerText) {
