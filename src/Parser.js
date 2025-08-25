b641843ca10fc294740ec68accbb731af0dbaa1c
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-24 19:10:11 -0500

    remove unused import

--- src/Parser.js
+++ src/Parser.js
@@ -1,14 +1,8 @@
 import parseRegistry from './utils/parseRegistry.js';
-import Debugger from './Debugger.js';
 import server from './Server.js';
 import stripTimestamp from './utils/stripTimestamp.js';
 
 class Parser {
-  constructor() {
-    this.debug = new Debugger();
-    this.debug.enable()
-  }
-
   readLine(line) {
     if (!line) return;
     server.send(stripTimestamp(line), 'log');
