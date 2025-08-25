96ad8a0052c1c88c7bcda109b956402879051e1c
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-24 19:09:44 -0500

    separate out timestamp regex

--- src/utils/stripTimestamp.js
+++ src/utils/stripTimestamp.js
@@ -1,3 +1,5 @@
-  export default function stripTimestamp(line) {
-    return line.replace(/^\[[A-Za-z]{3} [A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}\]\s*/, '').trim();
-  }
+const timestampRegex = /^\[[A-Za-z]{3} [A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}\]\s*/;
+  
+export default function stripTimestamp(line) {
+  return line.replace(timestampRegex, '').trim();
+}
