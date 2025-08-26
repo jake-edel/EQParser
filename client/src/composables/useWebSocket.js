8a6bfd78ada6cf7da106528f3b1c4d8464fd8fc2
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-25 21:29:19 -0500

    reorder module

--- client/src/composables/useWebSocket.js
+++ client/src/composables/useWebSocket.js
@@ -1,13 +1,20 @@
 import { reactive } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
 const socketListeners = reactive([])
+
 export default function useWebSocket(listeners) {
   const host = '192.168.1.79';
   // const host = '192.168.1.72';
   const serverPort = 4000;
   const socketAddress = `ws://${host}:${serverPort}/ws`;
-  
+
   let websocket;
+  function createWebSocket() {
+    websocket = new WebSocket(socketAddress);
+    websocket.onopen = () => console.log('WebSocket connection established');
+    return websocket;
+  }
+  
   function handleWebSocketRetry() {
     const interval = setInterval(() => {
       if (websocket?.readyState === WebSocket.OPEN) {
@@ -19,29 +26,18 @@ export default function useWebSocket(listeners) {
       console.log('Attempting to connect...');
     }, 2000);
   }
-
-  function createWebSocket() {
-    websocket = new WebSocket(socketAddress);
-    websocket.onopen = () => console.log('WebSocket connection established');
-    return websocket;
-  }
   
-  function startWebSocketService() {
-    console.log('Begin polling for WebSocket Connection');
-    handleWebSocketRetry();
-  }
-  
-  socketListeners.push(...listeners)
   function attachWsHandlers(websocket) {
     websocket.onclose = () => onWebSocketClose();
     websocket.onmessage = (event) => onWebSocketMessage(event);
   }
 
-  const onWebSocketClose = () => {
+    const onWebSocketClose = () => {
     console.log('WebSocket connection closed');
     handleWebSocketRetry();
   }
 
+  socketListeners.push(...listeners)
   async function onWebSocketMessage(event) {
     if (typeof event.data === 'string') {
       console.log('Received message:', event.data);
@@ -57,6 +53,11 @@ export default function useWebSocket(listeners) {
       });
     });
   };
+
+  function startWebSocketService() {
+    console.log('Begin polling for WebSocket Connection');
+    handleWebSocketRetry();
+  }
 
   return {
     startWebSocketService
