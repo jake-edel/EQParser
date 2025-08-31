cd0aa686b140fc193cd9590763cddc9941565467
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-31 11:12:24 -0500

    refine websocket retry logic

--- client/src/composables/useWebSocket.js
+++ client/src/composables/useWebSocket.js
@@ -14,39 +14,63 @@ export default function useWebSocket(listeners = []) {
   })
 
   let websocket;
-  function createWebSocket() {
-    websocket = new WebSocket(socketAddress);
-    websocket.onopen = () => console.log('WebSocket connection established');
-    return websocket;
-  }
-  
-  function handleWebSocketRetry() {
-    const interval = setInterval(() => {
-      if (websocket?.readyState === WebSocket.OPEN) {
-        clearInterval(interval);
-        attachWsHandlers(websocket);
-        return;
-      }
-      // Close previous connection before attempting another
-      if (websocket) websocket.close()
-      websocket = createWebSocket();
-      console.log('Attempting to connect...');
-    }, 2000);
-  }
-  
-  function attachWsHandlers(websocket) {
-    websocket.onclose = () => onWebSocketClose();
-    websocket.onmessage = (event) => onWebSocketMessage(event);
-  }
-
-  const onWebSocketClose = () => {
-    console.log('WebSocket connection closed');
-    handleWebSocketRetry();
-  }
 
   function startWebSocketService() {
     console.log('Begin polling for WebSocket Connection');
-    handleWebSocketRetry();
+    createWebSocket();
+    setTimeout(() => {
+      // If that connection does not open up
+      // within 3 seconds, begin retry loop
+      if (websocket.readyState !== WebSocket.OPEN) {
+        websocket.close()
+        handleWebSocketRetry()
+      }
+    }, 3000)
+  }
+
+  function createWebSocket() {
+    websocket = new WebSocket(socketAddress);
+    websocket.onopen = () => {
+      console.log('WebSocket connection established')
+      attachWsHandlers(websocket)
+    };
+
+    return websocket;
+  }
+
+  function attachWsHandlers(websocket) {
+    websocket.onmessage = (event) => onWebSocketMessage(event);
+    websocket.onclose = onWebSocketClose;
+  }
+  
+  function handleWebSocketRetry() {
+    // Immediately start up a new connection
+    createWebSocket()
+    // Start our retry loop
+    const interval = setInterval(async () => {
+      // If the connection is made within 5 seconds
+      // cancel the loop, attach the handlers, move along
+      if (websocket?.readyState === WebSocket.OPEN) {
+        clearInterval(interval);
+        attachWsHandlers(websocket)
+        return;
+      }
+      // Otherwise, close the existing attempt and start a new one
+      console.log('Attempting to re-connect...');
+      await websocket.close()
+      websocket = new WebSocket(socketAddress);
+      // Immediately attach the message handler
+      // so we can start receiving messages as soon as the connection is made
+      // Don't attach the close handler until the connection is made
+      // to prevent a hell loop when websocket.close() spawns new
+      // timeout loops.
+      websocket.onmessage = onWebSocketMessage
+    }, 3000);
+  }
+  
+  function onWebSocketClose() {
+    console.log('Connection closed')
+    handleWebSocketRetry()
   }
 
   async function onWebSocketMessage(event) {
