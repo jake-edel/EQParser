37de1206d5bac5975b5fd11db6a2e0cbc975f8fd
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-01 06:49:17 -0500

    refine multi-chat logic

--- client/src/components/ChatBox.vue
+++ client/src/components/ChatBox.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="card" style="margin-bottom: 16px;">
     <div style="display: flex; gap: 8px;">
-      <h2>{{ currentLog }}</h2>
+      <h2>{{ currentChat }}</h2>
       <button
         v-for="type in chatTypes"
         :key="type"
         type="button"
-        @click="() => currentLog = type"
+        @click="() => currentChat = type"
       >
         {{ type }}
       </button>
@@ -15,9 +15,9 @@
     </div>
     <div class="chat-container">
       <LogLine
-        v-for="(message, index) in logs[currentLog]"
+        v-for="(message, index) in chats[currentChat]"
         style="margin: 0;"
-        :key="message + index"
+        :key="uuid()"
         :message
       />
     </div>
@@ -29,35 +29,32 @@ import { reactive, ref } from 'vue';
 import useWebSocket from '../composables/useWebSocket';
 import LogLine from './LogLine.vue';
 
-const currentLog = ref('')
-
+const uuid = () => crypto.randomUUID()
 const chatTypes = ['debug', 'log', 'auction']
-
-const logs = reactive({})
-const logIsReversed = ref(false);
-
-const handleLogMessage = (message) => {
-  logIsReversed.value ? log.value.unshift(message) : log.value.push(message);
-}
+const currentChat = ref('')
+const chats = reactive({})
+const chatIsReversed = ref(false);
 
 function handleChatMessage(type, message) {
-  if (logs[type]) {
-    logs[type].push(message)
+  if (chats[type]) {
+    chatIsReversed.value 
+      ? chats[currentChat].value.unshift(message) 
+      : chats[currentChat].value.push(message);
   } else {
-    logs[type] = [message]
+    chats[type] = [message]
   }
 }
 
-useWebSocket([
-  { auction: (message) => handleChatMessage('auction', message) },
-  { debug: (message) => handleChatMessage('debug', message) },
-  { log: (message) => handleChatMessage('log', message) }
-]);
+const socketListeners = chatTypes.map((type) => ({
+  type: (message) => handleChatMessage(type, message)
+}))
+
+useWebSocket(socketListeners);
 
 const reverseLog = () => {
-  log.value = log.value.slice().reverse();
-  logIsReversed.value = !logIsReversed.value;
+  chats[currentChat].value = chats[currentChat].value.slice().reverse();
+  chatIsReversed.value = !chatIsReversed.value;
 }
 
-const clearLog = () => log.value.length = 0;
+const clearLog = () => chats[currentChat].value.length = 0;
 </script>
