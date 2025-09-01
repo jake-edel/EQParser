e624e596a822ac726228c2ab36379fd8a491217d
Jakob Edelstein <jakobedelstein@gmail.com>
2025-08-31 21:35:35 -0500

    adds compass component

---
+++ client/src/components/Compass.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex-col items-center">
+    {{ zonee }}
+    <div class="compass-grid">
+      <div
+        v-for="direction in directions" :key="direction"
+        :style="{
+          color: `${currentDirection === direction.key ? 'green': 'white'}`,
+          textAlign: 'center',
+          border: `solid 1px ${currentDirection === direction.key ? 'green': 'white'}`,
+          borderRadius: '50%',
+          padding: '2px',
+          margin: '2px'
+        }"
+      >
+        {{ direction.label }}
+      </div>
+    </div>
+    {{ location.x }} | {{ location.y }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import useWebSocket from '../composables/useWebSocket';
+
+const directions = [
+  {key: 'northwest', label: 'NW'},
+  {key: 'north', label: 'N'},
+  {key: 'northeast', label: 'NE'},
+  {key: 'west', label: 'W'},
+  {key: '', label: ''},
+  {key: 'east', label: 'E'},
+  {key: 'southwest', label: 'SW'},
+  {key: 'south', label: 'S'},
+  {key: 'southeast', label: 'SE'}
+]
+
+const currentDirection = ref('')
+
+const location = ref({ y: 0, x: 0 });
+const zonee = ref('');
+
+const handleCoordinates = (coordinates) => {
+  const { y, x } = coordinates;
+  location.value = { x, y };
+};
+
+const socketHandlers = [
+  { location: handleCoordinates },
+  { zone: (newZone) => { zonee.value = newZone; } },
+  { compassDirection: (direction) => { currentDirection.value = direction.toLowerCase() } }
+]
+useWebSocket(socketHandlers)
+
+</script>
+
+<style scoped>
+.compass-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
