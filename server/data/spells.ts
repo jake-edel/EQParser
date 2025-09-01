910312dc99390d98b83d82cf68cad2c6923c8e7e
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-01 06:02:51 -0500

    wip - spells

---
+++ server/data/spells.ts
@@ -0,0 +1,16 @@
+export default {
+  malignant_dead: {
+    name: 'Malignant Dead',
+    castTime: 14,
+    signature: 'At your service master',
+    type: 'pet',
+    maxHit: 39
+  },
+  spirit_of_wolf: {
+    name: 'Spirit of Wolf',
+    castTime: 0,
+    signature: 'You feel the spirit of wolf enter you.',
+    type: 'buff',
+    buffLength: 40
+  }
+}
