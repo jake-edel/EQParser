362f04c4dc6cfb46286ef71679174aae327a7219
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-01 22:10:16 -0500

    wip - update spell list

--- server/data/spells.ts
+++ server/data/spells.ts
@@ -1,12 +1,18 @@
 export const defaultSpell = {
-  name: ''
-}
+    name: '',
+    id: '',
+    type: '',
+    duration: 0,
+    onSpellLand: '',
+    onWearOff: '',
+    instanceId: ''
+  }
 
 export const spells = {
   malignant_dead: {
     name: 'Malignant Dead',
     id: 'malignant_dead',
-    signature: 'At your service master',
+    onSpellLand: 'At your service master',
     castTime: 14,
     type: 'pet'
   },
@@ -14,29 +20,29 @@ export const spells = {
     name: 'Spirit of Wolf',
     id: 'spirit_of_wolf',
     castTime: 0,
-    signature: 'You feel the spirit of wolf enter you.',
+    onSpellLand: 'You feel the spirit of wolf enter you.',
     type: 'buff',
     duration: 40
   },
   deadeye: {
     name: 'Deadeye',
     id: 'deadeye',
-    signature: 'Your vision shifts.',
+    onSpellLand: 'Your vision shifts.',
     type: 'buff',
-    duration: 0
-
+    duration: 24
   },
   shadow_sight: {
     name: 'Shadow Sight',
     id: 'shadow_sight',
-    signature: 'The shadows fade.',
+    onSpellLand: 'The shadows fade.',
+    onWearOff: 'Your shadow sight fades.',
     type: 'buff',
-    duration: 0
+    duration: 27
   },
   gather_shadows: {
     name: 'Gather Shadows',
     id: 'gather_shadows',
-    signature: 'You gather shadows about you.',
+    onSpellLand: 'You gather shadows about you.',
     castTime: 5,
     wearOff: 'Your shadows fade',
     type: 'buff',
@@ -46,23 +52,39 @@ export const spells = {
   augment_death: {
     name: 'Augment Death',
     id: 'augment_death',
-    signature: 'gleam with madness',
-    duration: [
-      {
-        level: 39,
-        duration: 12.7
-      },
-      {
-        level: 47,
-        duration: 15
-      }
-    ]
+    onSpellLand: 'gleam with madness',
+    // duration: [
+    //   {
+    //     level: 39,
+    //     duration: 12.7
+    //   },
+    //   {
+    //     level: 47,
+    //     duration: 15
+    //   }
+    // ]
+    duration: 12.7
   },
   panic_the_dead: {
     name: 'Panic the dead',
     id: 'panic_the_dead',
-    signature: 'has the fear of life put',
+    onSpellLand: 'has the fear of life put',
     castTime: 2,
-    duration: 54
-  }
+    duration: 0.9
+  },
+  call_of_bones: {
+    name: 'Call of Bones',
+    id: 'call_of_bones',
+    onSpellLand: 'You feel the skin peel from your bones.',
+    castTime: 0,
+    duration: 11 // 11:12 base + 18sec/level above 34
+  },
+  vampiric_curse: {
+    name: 'Vampiric Curse',
+    id: 'vampiric_curse',
+    onSpellLand: 'pales.',
+    castTime: 0,
+    duration: 0.9
+  },
+  
 }
