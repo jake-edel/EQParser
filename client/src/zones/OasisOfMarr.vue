6cb7ab793255e03c36634e0ef634f2decd936f80
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-02 20:05:30 -0500

    add wiki link

--- client/src/zones/OasisOfMarr.vue
+++ client/src/zones/OasisOfMarr.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li>1. <a href="/Ogre" title="Ogre">Ogre</a> Temple surrounded by Orcs</li>
+    <li>1. <a href="https://wiki.project1999.com/Ogre" target="_blank" title="Ogre">Ogre</a> Temple surrounded by Orcs</li>
     <li>2. Abandoned Hut and Dock with boats to Kunark</li>
     <li>
       3. Gypsy Camp with Combine Weapons, Food and Goods, Shaman Spells, and other
