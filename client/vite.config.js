7ab1e4d16d86a2765204009ad9a7b60c3ee78c0f
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-03 08:25:53 -0500

    move vite config into client directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,14 @@
 /** @type {import('vite').UserConfig} */
 import vue from '@vitejs/plugin-vue'
+// TODO: load env from vite without needing dotenv
 import 'dotenv/config'
 
 process.env.BROWSER = 'chrome'
 
 export default {
-
   root: './client',
   server: {
     port: 3000,
-    open: '/dashboard',
-    watch: './client',
     proxy: {
       '/ws': {
         target: `ws://${process.env.HOST_IP}:4000`,
