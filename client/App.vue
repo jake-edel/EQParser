8bf507897f1e450e499bdc2fbe4f5eaade396a03
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-02 21:20:16 -0500

    add tab selectors for main component

--- client/App.vue
+++ client/App.vue
@@ -4,12 +4,28 @@
     <Compass />
     <CoinCounter />
   </div>
-  <SpellList />
-  <ZoneMap />
-  <ChatBox />
+  <div class="flex">
+    <button
+      v-for="component in components"
+      :key="component"
+      @click="activeComponent = component"
+      :style="{
+          backgroundColor: component === activeComponent ? 'blue' : 'white',
+          width: '64px',
+          height: '24px',
+          borderRadius: '8px'
+      }"
+    >
+      {{ component }}
+    </button>
+  </div>
+  <SpellList v-show="activeComponent === 'spells'" />
+  <ZoneMap v-show="activeComponent === 'zone'" />
+  <ChatBox v-show="activeComponent === 'chat'" />
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import useWebSocket from './src/composables/useWebSocket.js'
 import CharacterInfo from './src/components/CharacterInfo.vue'
 import Compass from './src/components/Compass.vue';
@@ -17,6 +33,9 @@ import CoinCounter from './src/components/CoinCounter.vue';
 import SpellList from './src/components/SpellList.vue';
 import ChatBox from './src/components/ChatBox.vue';
 import ZoneMap from './src/components/ZoneMap.vue';
+
+const components = ['spells', 'zone', 'chat']
+const activeComponent = ref('chat')
 
 const { startWebSocketService } = useWebSocket();
 startWebSocketService();
