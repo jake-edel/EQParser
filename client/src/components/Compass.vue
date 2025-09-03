b9bb6cbd883beef5b33941ca720efc96bec03124
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-03 08:06:24 -0500

    formatting

--- client/src/components/Compass.vue
+++ client/src/components/Compass.vue
@@ -5,19 +5,18 @@
     <div class="compass-grid">
       <div v-for="direction, index in directions" :key="direction">
         <div v-if="index === 4" />
-        <div v-else
-          class="flex items-center"
-          :style="{
-            color: `${currentDirection === direction.key ? 'green': 'white'}`,
-            rotate: `${direction.rotation}deg`,
-            height: '10px',
-            width: '10px',
-            padding: '2px',
-            margin: '2px'
-        }"
-        >
-          <svg viewBox="0 0 100 100" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
-            <polygon points="50,0 100,100 0,100" :fill="currentDirection === direction.key ? 'green': 'white'" stroke="black" stroke-width="1"/>
+        <div v-else class="flex items-center" :style="{
+          color: `${currentDirection === direction.key ? 'green' : 'white'}`,
+          rotate: `${direction.rotation}deg`,
+          height: '10px',
+          width: '10px',
+          padding: '2px',
+          margin: '2px'
+        }">
+          <svg viewBox="0 0 100 100" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid meet">
+            <polygon points="50,0 100,100 0,100" :fill="currentDirection === direction.key ? 'green' : 'white'"
+              stroke="black" stroke-width="1" />
           </svg>
         </div>
       </div>
@@ -33,15 +32,15 @@ import { ref } from 'vue';
 import useWebSocket from '../composables/useWebSocket';
 
 const directions = [
-  {rotation: 315 },
-  {rotation: 0},
-  {rotation: 45},
-  {rotation: 270},
+  { rotation: 315 },
+  { rotation: 0 },
+  { rotation: 45 },
+  { rotation: 270 },
   {},
-  {rotation: 90},
-  {rotation: 225 },
-  {rotation:  180},
-  {rotation: 135}
+  { rotation: 90 },
+  { rotation: 225 },
+  { rotation: 180 },
+  { rotation: 135 }
 ]
 
 const currentDirection = ref('')
