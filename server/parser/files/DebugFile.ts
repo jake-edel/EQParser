b5ec5a5e988aa8f7e66827a538d2f5af0e420592
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-02 21:27:10 -0500

    ignore debug log

--- server/parser/files/DebugFile.ts
+++ server/parser/files/DebugFile.ts
@@ -2,7 +2,7 @@ import File from "./File.ts";
 
 class DebugFile extends File {
   constructor() {
-    super('C:\\Users\\Jake\\Documents\\Code\\EQParser\\server\\data\\debug.txt')
+    super('C:\\Users\\Jake\\Documents\\Code\\EQParser\\server\\data\\debug.log')
   }
 
   async write(line: string) {
