55de60865e0c1deb803047c5ab066d06864f2c04
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-03 17:54:47 -0500

    load env file without external dependency

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,27 @@
 /** @type {import('vite').UserConfig} */
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// TODO: load env from vite without needing dotenv
-import 'dotenv/config'
 
 process.env.BROWSER = 'chrome'
 
-export default {
-  root: './client',
-  server: {
-    port: 3000,
-    proxy: {
-      '/ws': {
-        target: `ws://${process.env.HOST_IP}:4000`,
-        ws: true,
-        rewriteWsOrigin: true
-      }
-    }
-  },
-  plugins: [vue({
-    isProduction: true,
-    optionsAPI: false,
-  })]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd() + '\\client', '')
 
-}
+  return {
+    root: './client',
+    server: {
+      port: 3000,
+      proxy: {
+        '/ws': {
+          target: `ws://${env.VITE_HOST_IP}:4000`,
+          ws: true,
+          rewriteWsOrigin: true
+        }
+      }
+    },
+    plugins: [vue({
+      isProduction: true,
+      optionsAPI: false,
+    })]
+  }
+})
