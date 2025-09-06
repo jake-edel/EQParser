7059b78b9490c2f597d5c909d0cd629b7aaa28cd
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-06 14:48:08 -0500

    configure app to run with https in production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,27 @@
 /** @type {import('vite').UserConfig} */
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import fs from 'fs'
-// import path from 'path'
+import fs from 'fs'
+import path from 'path'
 
 process.env.BROWSER = 'chrome'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd() + '\\client', '')
 
+  let https = {}
+  if (mode === 'production') {
+    https = {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost+1-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost+1.pem'))
+    }
+  }
+
   return {
     root: './client',
     server: {
       port: 3000,
-      // https: {
-      //   key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost+1-key.pem')),
-      //   cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost+1.pem'))
-      // },
+      https,
       host: '0.0.0.0',
       proxy: {
         '/ws': {
