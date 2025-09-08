53d67d74798e3948c70acd1b1e0f1b40818b17b0
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-07 21:29:49 -0500

    read location from file on server start

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,9 @@
 import LogWatcher from './parser/LogWatcher.ts';
 import server from './parser/Server.ts';
+import location from './parser/modules/Location.ts';
 
 server.start();
 
 await new LogWatcher().startWatchingLog();
+
+await location.searchForCurrentZone()
