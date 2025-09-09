46642e70145f302b81f9d9e51893642c284003d2
Jakob Edelstein <jakobedelstein@gmail.com>
2025-09-08 20:17:56 -0500

    format time on timer

--- client/src/components/SpellTimer.vue
+++ client/src/components/SpellTimer.vue
@@ -2,19 +2,34 @@
   <button
     type="button"
     class="flex items-center"
-    style="width: min-content; background: none;"
+    style="width: fit-content; background: none; color: white;"
     @click="$emit('cancelTimer', spell)"
   >
     <img :src="`../../assets/icons/${spell.icon}.png`" alt="spell icon"/>
-    {{ spell.name }} {{ spell.timeRemaining }}
+    <div class="flex-col">
+      <div>
+        {{ spell.name }}
+      </div>
+      <div>
+        {{ timer }}
+      </div>
+    </div>
   </button>
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
   spell: {
     type: Object,
     required: true
   }
+})
+
+const timer = computed(() => {
+  const mins = (props.spell.timeRemaining / 60).toFixed()
+  const seconds = props.spell.timeRemaining % 60
+  return `${mins}:${seconds <= 9 ? '0' : ''}${seconds}`
 })
 </script>
